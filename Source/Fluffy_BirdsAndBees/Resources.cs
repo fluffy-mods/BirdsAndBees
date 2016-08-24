ed72170ffeb83af7a903bfec31324ef8bbf67641
FluffierThanThou <fluffy.l2032@gmail.com>
2016-08-24 19:38:03 +0200

    fix thoughtDef name, add preview image and steam release.

--- Source/Fluffy_BirdsAndBees/Resources.cs
+++ Source/Fluffy_BirdsAndBees/Resources.cs
@@ -30,7 +30,7 @@ namespace Fluffy_BirdsAndBees
                                                                          }
                                                                  };
 
-        public static ThoughtDef failedLovingThoughtDef_Male = ThoughtDef.Named( "FailedLovingMale" );
+        public static ThoughtDef failedLovingThoughtDef_Male = ThoughtDef.Named( "FailedLovinMale" );
         public static ThoughtDef failedLovingThoughtDef_Female = ThoughtDef.Named( "FailedLovinFemale" );
 
         public static void Debug( string text, int indent = 0 )
