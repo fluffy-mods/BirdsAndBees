04392f868af629f90606ed75c702509ea2682be7
FluffierThanThou <fluffy.l2032@gmail.com>
2017-06-05 11:16:26 +0200

    make efficiency of reproductive organs actually have an effect on fertility

---
+++ Source/Fluffy_BirdsAndBees/PawnCapacityWorker_Fertility.cs
@@ -0,0 +1,22 @@
+﻿// Karel Kroeze
+// PawnCapacityWorker_Fertility.cs
+// 2017-06-05
+
+using System.Collections.Generic;
+using Verse;
+
+namespace Fluffy_BirdsAndBees
+{
+    public class PawnCapacityWorker_Fertility : PawnCapacityWorker
+
+    {
+        public override float CalculateCapacityLevel( HediffSet diffSet,
+                                                      List<PawnCapacityUtility.CapacityImpactor> impactors = null )
+        {
+            return PawnCapacityUtility.CalculateTagEfficiency( diffSet, "FertilitySource", 3.40282347E+38f, impactors );
+        }
+
+        public override bool CanHaveCapacity( BodyDef body ) { return body.HasPartWithTag("FertilitySource"); }
+    }
+}
+
