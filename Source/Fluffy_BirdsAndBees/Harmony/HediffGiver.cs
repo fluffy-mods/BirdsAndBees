9d3d9cc258e0d0cb674915cf345d65f1b4988b61
Fluffy <fluffy.l2032@gmail.com>
2018-06-14 21:25:57 +0200

    initial v1.0 update

--- Source/Fluffy_BirdsAndBees/Harmony/HediffGiver.cs
+++ Source/Fluffy_BirdsAndBees/Harmony/HediffGiver.cs
@@ -14,8 +14,9 @@ namespace Fluffy_BirdsAndBees
         static bool Prefix( HediffGiver __instance, ref bool __result, Pawn pawn )
         {
             // if the instance is of gendered type, check pawn gender before applying.
-            Debug( "HediffGiver.TryApply(" + __instance.hediff.defName + ", " + pawn.Name.ToStringShort + " [" +
-                             pawn.gender + "])" );
+            // NOTE: This debug call can give nullref errors in certain cases (what cases?). Disabled for now.
+            // Debug( "HediffGiver.TryApply(" + __instance.hediff.defName + ", " + pawn.Name.ToStringShort + " [" +
+            //         pawn.gender + "])" );
             HediffGiver_Birthday_Gender gendered = __instance as HediffGiver_Birthday_Gender;
             if ( gendered != null && gendered.gender != pawn.gender )
             {
