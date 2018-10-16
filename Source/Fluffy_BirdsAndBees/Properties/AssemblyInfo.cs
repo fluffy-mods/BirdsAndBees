abcf54e9d2a79af5c4011a4b5928acaf4c0f1a2a
Fluffy <fluffy.l2032@gmail.com>
2018-10-16 17:28:26 +0200

    'Release 2.0.43 [nolog]'

--- Source/Fluffy_BirdsAndBees/Properties/AssemblyInfo.cs
+++ Source/Fluffy_BirdsAndBees/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using Verse;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.10.40")]
-[assembly: AssemblyFileVersion("1.10.40")]
+[assembly: AssemblyVersion("2.0.43")]
+[assembly: AssemblyFileVersion("2.0.43")]
