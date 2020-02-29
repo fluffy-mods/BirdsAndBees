b0193da815ce37259f64ee59ab9b378fd6cc8fa2
Fluffy <fluffy.l2032@gmail.com>
2020-03-01 00:35:29 +0100

    fix lovin thoughts

--- Source/Fluffy_BirdsAndBees/Harmony/JobDriver_Lovin_MakeNewToils_FinishAction.cs
+++ Source/Fluffy_BirdsAndBees/Harmony/JobDriver_Lovin_MakeNewToils_FinishAction.cs
@@ -12,61 +12,35 @@ using static HarmonyLib.AccessTools;
 
 namespace Fluffy_BirdsAndBees
 {
-    [HarmonyPatch]
+    [HarmonyPatch( typeof( JobDriver_Lovin ), "<MakeNewToils>b__11_4")]
     public static class JobDriver_Lovin_MakeNewToils_FinishAction
     {
-        public const string ITERATOR_NAME = "<MakeNewToils>c__Iterator0";
-        public const string FINISH_ACTION_NAME = "<>m__4";
-        public const string DRIVER_FIELD_NAME = "$this";
-
-        static MethodInfo TargetMethod()
-        {
-            var iterator = Inner( typeof( JobDriver_Lovin ), ITERATOR_NAME );
-            if( iterator == null )
-                throw new NullReferenceException( "iterator null" );
-
-            var finish_action = Method( iterator, FINISH_ACTION_NAME );
-            if ( finish_action == null )
-                throw new NullReferenceException( "finish action null" );
-
-            return finish_action;
-        }
-
         [Conditional("DEBUG_JOBDRIVER_LOVIN")]
         static void Assert( object obj, string name )
         {
             Log.Message( $"{name} is { obj ?? "NULL" }" );
         }
 
-        static bool Prefix( object __instance )
+        static bool Prefix( JobDriver_Lovin __instance )
         {
-            Assert(__instance, "instance");
-            var traverse = Traverse.Create( __instance );
-            Assert( traverse, "traverse" );
-            var driver = traverse.Field( DRIVER_FIELD_NAME ).GetValue<JobDriver_Lovin>();
-            Assert(driver, "driver");
-
-            var driverTraverse = Traverse.Create( driver );
+            var driverTraverse = Traverse.Create( __instance );
             Assert(driverTraverse, "driverTraverse");
             var partner = driverTraverse.Property( "Partner" ).GetValue<Pawn>();
             Assert(partner, "partner");
 
-            var genTicksToNextLovin = driverTraverse.Method( "GenerateRandomMinTicksToNextLovin", driver.pawn );
+            var genTicksToNextLovin = driverTraverse.Method( "GenerateRandomMinTicksToNextLovin", __instance.pawn );
             Assert( genTicksToNextLovin, "GenTicksMethod" );
-            var ticksToNextLovin = genTicksToNextLovin.GetValue<int>( driver.pawn );
+            var ticksToNextLovin = genTicksToNextLovin.GetValue<int>( __instance.pawn );
             Assert(ticksToNextLovin, "ticksToNextLovin"  );
 
-            var performanceLevel = LovinUtility.LovinLevel( driver.pawn, partner );
+            var performanceLevel = LovinUtility.LovinLevel( __instance.pawn, partner );
 
             // apply the thought
             var thought = ThoughtMaker.MakeThought( ThoughtDefOf.LovinPerformance, performanceLevel );
-            // note that we have to massage the thought a bit due to a bug in vanilla;
-            // https://ludeon.com/forums/index.php?topic=33052
-            ((Thought_MemorySocial)thought).opinionOffset = thought.CurStage.baseOpinionOffset;
-            driver.pawn.needs.mood.thoughts.memories.TryGainMemory( thought, partner );
+            __instance.pawn.needs.mood.thoughts.memories.TryGainMemory( thought, partner );
 
             // we're civilized, not bunnies.
-            driver.pawn.mindState.canLovinTick = Find.TickManager.TicksGame + ticksToNextLovin;
+            __instance.pawn.mindState.canLovinTick = Find.TickManager.TicksGame + ticksToNextLovin;
 
             return false;
         }
