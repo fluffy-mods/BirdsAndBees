2d4362856e5e288e426bfe8be026fdaa6386314b
Fluffy <fluffy.l2032@gmail.com>
2020-03-05 22:40:31 +0100

    'Release 3.5.54 [nolog]'

--- Source/Fluffy_BirdsAndBees/Properties/AssemblyInfo.cs
+++ Source/Fluffy_BirdsAndBees/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using Verse;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.4.53")]
+[assembly: AssemblyFileVersion("3.5.54")]
